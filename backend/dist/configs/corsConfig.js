7ed4aaaf994067cb08ee1d6b466910c53da9e6e5
codecreatorsss <codecreatorsss@gmail.com>
2024-06-05 00:28:56 +0530

    added new log

--- backend/dist/configs/corsConfig.js
+++ backend/dist/configs/corsConfig.js
@@ -8,6 +8,7 @@ const whitelist = [
 // Configure CORS options
 exports.corsOptions = {
     origin: function (origin, callback) {
+        console.log("origin", origin);
         // Check if the origin is in the whitelist or if it's a local request
         if (whitelist.includes(origin) || !origin) {
             callback(null, true);
